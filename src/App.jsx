b03d9a8e0ee79e62815d82dad106ac293c34a635
autoservicio2-up-edu-pe <aralay.p@gmail.com>
2025-01-14 18:12:09 -0500

    test

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="mt-10 flex justify-center hover:justify-start">TAILWINDCSS TEST WITH GITHUBPAGES</h1>
+    </>
+  );
+}
+
+export default App;
